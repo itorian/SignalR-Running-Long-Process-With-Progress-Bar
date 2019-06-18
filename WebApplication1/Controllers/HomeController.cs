f108bf775ffe6d03d0c67b66bad48d50d9cfc1ea
Abhimanyu Kumar Vatsa <itorian@live.in>
2019-06-18 19:35:59 +0530

    init

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Web.Mvc;
+using System.Threading;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void StartProgress([Bind(Prefix = "id")] string connectionId)
+        {
+            var hub = new ProgressHub();
+
+            hub.NotifyStart(connectionId);
+
+
+            // updating client progress
+            Thread.Sleep(1000);
+            hub.NotifyProgress(connectionId, 10);
+
+            Thread.Sleep(1000);
+            hub.NotifyProgress(connectionId, 20);
+
+            Thread.Sleep(1000);
+            hub.NotifyProgress(connectionId, 30);
+
+            Thread.Sleep(1000);
+            hub.NotifyProgress(connectionId, 40);
+
+            Thread.Sleep(1000);
+            hub.NotifyProgress(connectionId, 50);
+
+            Thread.Sleep(1000);
+            hub.NotifyProgress(connectionId, 60);
+
+            Thread.Sleep(1000);
+            hub.NotifyProgress(connectionId, 70);
+
+            Thread.Sleep(1000);
+            hub.NotifyProgress(connectionId, 80);
+
+            Thread.Sleep(1000);
+            hub.NotifyProgress(connectionId, 90);
+
+            Thread.Sleep(1000);
+            hub.NotifyProgress(connectionId, 100);
+
+
+            Thread.Sleep(1000);
+            hub.NotifyEnd(connectionId);
+        }
+    }
+}
---
+++ WebApplication1/ProgressHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace WebApplication1
+{
+    public class ProgressHub : Hub
+    {
+        public void NotifyStart(string connectionId)
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<ProgressHub>();
+            hubContext.Clients.Client(connectionId).initProgressBar(connectionId);
+        }
+
+        public void NotifyProgress(string connectionId, int percentage)
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<ProgressHub>();
+            hubContext.Clients.Client(connectionId).updateProgressBar(connectionId, percentage);
+        }
+
+        public void NotifyEnd(string connectionId)
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<ProgressHub>();
+            hubContext.Clients.Client(connectionId).clearProgressBar(connectionId);
+        }
+    }
+}
