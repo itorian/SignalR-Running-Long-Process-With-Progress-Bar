d0910feaeb9759fddf45874e668c69fdbbe02597
Abhimanyu Kumar Vatsa <itorian@live.in>
2019-06-19 11:33:59 +0530

    using task.delay

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using System.Web.Mvc;
-using System.Threading;
+using System.Threading.Tasks;
 
 namespace WebApplication1.Controllers
 {
@@ -10,46 +10,17 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public void StartProgress([Bind(Prefix = "id")] string connectionId)
+        public async Task StartProgress([Bind(Prefix = "id")] string connectionId)
         {
             var hub = new ProgressHub();
-
             hub.NotifyStart(connectionId);
 
+            for (int i = 0; i <= 100; i += 5)
+            {
+                hub.NotifyProgress(connectionId, i);
+                await Task.Delay(100);
+            }
 
-            // updating client progress
-            Thread.Sleep(1000);
-            hub.NotifyProgress(connectionId, 10);
-
-            Thread.Sleep(1000);
-            hub.NotifyProgress(connectionId, 20);
-
-            Thread.Sleep(1000);
-            hub.NotifyProgress(connectionId, 30);
-
-            Thread.Sleep(1000);
-            hub.NotifyProgress(connectionId, 40);
-
-            Thread.Sleep(1000);
-            hub.NotifyProgress(connectionId, 50);
-
-            Thread.Sleep(1000);
-            hub.NotifyProgress(connectionId, 60);
-
-            Thread.Sleep(1000);
-            hub.NotifyProgress(connectionId, 70);
-
-            Thread.Sleep(1000);
-            hub.NotifyProgress(connectionId, 80);
-
-            Thread.Sleep(1000);
-            hub.NotifyProgress(connectionId, 90);
-
-            Thread.Sleep(1000);
-            hub.NotifyProgress(connectionId, 100);
-
-
-            Thread.Sleep(1000);
             hub.NotifyEnd(connectionId);
         }
     }
